f80acae44e435ef970f45caa03698f13904c6ad0
Jan Mennen <janmennen@gmx.de>
2017-07-12 10:15:44 +0200

    API Gateaway added

--- sls-backend/handler.js
+++ sls-backend/handler.js
@@ -20,7 +20,7 @@ module.exports.notify = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Go Serverless v1.0! Your function notify executed successfully!',
       input: event,
     }),
   };
