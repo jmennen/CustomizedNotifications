f79a61a233994f11e50d7f5f08fec21f6d5529c3
Jan Mennen <janmennen@gmx.de>
2017-07-12 15:56:08 +0200

    Added a first functionality to send messagaes to an SNS topic

--- sls-backend/handler.js
+++ sls-backend/handler.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const SNS = new AWS.SNS();
+
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
@@ -24,6 +27,31 @@ module.exports.notify = (event, context, callback) => {
       input: event,
     }),
   };
+
+
+  var params = {
+    Message: 'Hello SNS I love to publish here :)', /* required */
+    // MessageAttributes: {
+      // '<String>': {
+      //   DataType: 'JSON', /* required */
+      //   BinaryValue: new Buffer('...') || 'STRING_VALUE',
+      //   StringValue: 'STRING_VALUE'
+      // },
+      /* '<String>': ... */
+    // },
+    // MessageStructure: 'STRING_VALUE',
+    // PhoneNumber: 'STRING_VALUE',
+     Subject: 'My first message',
+    // TargetArn: 'STRING_VALUE',
+    TopicArn: 'arn:aws:sns:us-east-1:095380879276:notifications'
+  };
+  SNS.publish(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+
+
+
 
   callback(null, response);
 
