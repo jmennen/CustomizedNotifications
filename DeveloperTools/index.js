6b98fd36978d6a52ed9577def87700065f0ad7cc
Jan Mennen <janmennen@gmx.de>
2017-08-02 18:32:29 +0200

    DeleteTopic, GetAllTopics & Notify functions implemented

--- DeveloperTools/index.js
+++ DeveloperTools/index.js
@@ -55,6 +55,103 @@ class CustomNotificationTools {
 
 
     /**
+     * Delete a topic
+     * 
+     * @param {string} id - The id of the new topic 
+     * @param {requestCallback} callback  - Callback function with either the error message (if the calls fails) or the deleted topic element (Name and ID) when the topic was deleted.
+     */
+    deleteTopic(id, callback){
+        if(typeof(id) === 'string'){
+            const params = {
+                 method: 'DELETE',
+                 uri: this.API_URL + 'topics/' + id
+            }
+
+            request(params, function (error, response, body) {
+                if(error){
+                    console.log('error:', error); // Print the error if one occurred 
+                    callback('Problem with request', null);
+                }else{
+                    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+                    console.log('body:', body); // Print the HTML for the Google homepage. 
+                    callback(null, JSON.parse(body));
+                }               
+            });
+
+        }else{
+            console.log('The deleteTopic() function requires a string as an input for id');
+            callback('The deleteTopic() function  requires a string as an input for id', null);
+        }  
+    }
+
+    /**
+     * Get all topics
+     * 
+     * @param {requestCallback} callback  - Callback function with either the error message (if the calls fails) or an array of topics (called "data", where each element contains the name and the id). 
+     */
+    getAllTopics(callback){
+        const params = {
+                method: 'GET',
+                uri: this.API_URL + 'topics/'
+        }
+
+        request(params, function (error, response, body) {
+            if(error){
+                console.log('error:', error); // Print the error if one occurred 
+                callback('Problem with request', null);
+            }else{
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+                console.log('body:', body); // Print the HTML for the Google homepage. 
+                callback(null, JSON.parse(body));
+            }               
+        });
+ 
+    }
+
+    /**
+     * Send a notification
+     * 
+     * @param {string} topicId - The id of the topic of the notification
+     * @param {string} subject - The subject of the notification
+     * @param {string} msg - The message of the notification
+     * @param {requestCallback} callback  - Callback function with either the error message (if the calls fails) or a success message if the notification gets delivered.
+     */
+    notify(topicId, subject, msg, callback){
+        if(typeof(topicId) === 'string' && typeof(subject) === 'string' && typeof(msg) === 'string'){
+            const content = JSON.stringify({
+                data : {
+                    topicId : topicId,
+                    subject : subject,
+                    message : msg
+                    }
+            });
+
+            
+            const params = {
+                 method: 'POST',
+                 uri: this.API_URL + 'notify/',
+                 body: content
+            }
+
+            request(params, function (error, response, body) {
+                if(error){
+                    console.log('error:', error); // Print the error if one occurred 
+                    callback('Problem with request', null);
+                }else{
+                    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+                    console.log('body:', body); // Print the HTML for the Google homepage. 
+                    callback(null, JSON.parse(body));
+                }               
+            });
+
+        }else{
+            console.log('The deleteTopic() function requires a string as an input for id');
+            callback('The deleteTopic() function  requires a string as an input for id', null);
+        }  
+    }
+
+
+    /**
      * The Frame of the callback function used by all methods in the CustomNotificationTools class.
      *
      * @callback requestCallback
